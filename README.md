e982e0ff8aaaecdd59c5809234b6e1766683b43c
arty <art.yerkes@gmail.com>
2014-09-20 03:31:33 -0700

    Initial revision of ymacs in atom.  Basic functionality is working (you can load save and edit and buffers are transferred in both directions).

---
+++ README.md
@@ -0,0 +1,13 @@
+# Ymacs in atom
+
+I like atom a lot in concept and in practice, but I'm an emacs user primarily.  To
+help me transition, I've written this thin wrapper around the wonderful ymacs editing
+widget https://github.com/mishoo/ymacs .  This will keep me in atom long enough to
+learn to script and use it.  Maybe it will be of value to someone else, too.
+
+Features:
+
+- ymacs will automatically attach to your current atom buffers.
+- C-x C-s in ymacs will transfer text back to your atom buffers.
+- Opening a new file in atom will open it in ymacs too if ymacs is open.
+- Killing a buffer in ymacs will release the edit object in atom.
---
+++ resources/ymacs-in-atom.js
@@ -0,0 +1,105 @@
+/*
+
+  Note that this file is just an example.  It should not be treated as
+  part of Ymacs itself.  Ymacs is just an editing platform and as such
+  it has no menus, no toolbar etc.  These can be easily added using
+  other DynarchLIB widgets, as this file demonstrates.
+
+  If a collection of useful menus/toolbars will emerge, a new compound
+  widget will be defined.
+
+*/
+
+var messageHandler;
+var messageData;
+
+function save_buffer() {
+    var text = this.code.join("\n");
+    window.parent.postMessage(
+        {type: "save", 
+         file: this.name,
+         text: text}, "file://");
+};
+
+$(document).ready(function() {
+    Ymacs_Buffer.newCommands({
+        save_buffer: Ymacs_Interactive(save_buffer)
+    });
+    
+    var desktop = new DlDesktop({});
+    var desktopElement = desktop.getElement();
+    desktopElement.parentNode.removeChild(desktopElement);
+    document.getElementById('ymacs-in-a-div').appendChild(desktopElement);
+    desktop.fullScreen();
+    desktopElement.id = "ymacs-desktop";
+    
+    var layout = new DlLayout({ parent: desktop });
+
+    var empty = new Ymacs_Buffer({ name: "empty" });
+    var ymacs = window.ymacs = new Ymacs({ buffers: [ empty ]});
+    ymacs.setColorTheme([ "dark", "y" ]);
+    
+    try {
+        ymacs.getActiveBuffer().cmd("eval_file", ".ymacs");
+    } catch(ex) {}
+    
+    messageHandler = function(event) {
+        var message;
+        if (event)
+            message = event.data;
+        else
+            message = window.messageData;
+
+        console.log("ymacs-iframe:message", message);
+        var type = message.type;
+        if (message.type === "new-buffer") {
+            var code = message.text;
+            var buf = ymacs.getBuffer(message.file) || ymacs.createBuffer({name:message.file});
+            buf.COMMANDS.kill_buffer = Ymacs_Interactive(function() {
+                this.whenYmacs(function(ymacs) {
+                    ymacs.killBuffer(this);
+                });
+                window.parent.postMessage({type: "killed", file:message.file}, "file://");
+            });
+            buf.setCode(code);
+            
+            // XXX arty set mode by file type
+            if (message.file.endsWith('.js'))
+                buf.cmd("javascript_mode");
+
+            ymacs.switchToBuffer(buf);
+            buf.keymap[0].defineKey("C-x C-s", save_buffer, "");
+        } else if (message.type === "change-buffer") {
+            var buf = ymacs.getBuffer(message.file);
+            if (buf) {
+                buf.setCode(message.text);
+            }
+        } else if (message.type === "delete-buffer") {
+            var buf = ymacs.getBuffer(message.file);
+            if (buf) {
+                ymacs.killBuffer(buf);
+            }
+        }
+    };
+
+    window.addEventListener("message", messageHandler);
+
+    layout.packWidget(ymacs, { pos: "bottom", fill: "*" });
+
+    $('#ymacs-desktop').css({'position':'relative'});
+    var ydiv = $('#ymacs-in-a-div');
+    function ymacs_size() {
+        console.log('size?');
+        var size = {x:ydiv.width(), y:ydiv.height()};
+        $('#ymacs-desktop').css('width',size.x+'px');
+        $('#ymacs-desktop').css('height',size.y+'px');
+        desktop.setSize(size);
+    };
+    $('#ymacs-in-a-div').resize(ymacs_size);
+    $(window).resize(ymacs_size);
+    ymacs_size();
+
+    DynarchDomUtils.trash(document.getElementById("x-loading"));
+
+    window.parent.postMessage({type: "loaded"}, "file://");
+});
